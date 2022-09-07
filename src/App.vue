42b8b141866761c39ea7758a7698ef1d72b59827
Phiwa Madinga <phiwa@koedr.org>
2022-09-07 12:15:43 +0200

    router history

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,27 @@
     <router-link :to="{name: 'about'}">About</router-link> | 
     <router-link :to="{ name: 'jobs'}">Jobs</router-link>
   </nav>
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go back</button>
+  <button @click="forward">Go forward</button>
   <router-view/>
 </template>
+
+<script>
+  export default{
+    methods: {
+      redirect(){
+        this.$router.push({ name: 'home'})
+      },
+      back() {
+        this.$router.go(-1)
+      },
+      forward(){
+        this.$router.go(1)
+      }
+    }
+  }
+</script>
 
 <style>
 #app {
@@ -31,6 +50,11 @@ nav a {
 nav a.router-link-exact-active {
   color: #fff;
   background-color: rgb(101, 50, 61);
-
+}
+button{
+  margin: 0 10px;
+  padding: 10px;
+  border: 0;
+  border-radius: 4px;
 }
 </style>
